543bd81a7345289b77fb9150554f1f889d1dd534
CharlesCaltagirone <charles.dewidehem-caltagirone@laplateforme.io>
2023-01-18 01:13:21 +0100

    jour2, job 05 corrigé

--- Jour02/job05/index.php
+++ Jour02/job05/index.php
@@ -11,11 +11,14 @@
 <body>
     <?php
     $x = 1000;
-    for ($a = 2; $a < $x; $a++) {
-        for ($b = 2; $b < $a; $b++) {
-            if (($a % $b) == 0) break;
-            if ($b == ($a - 1))
-                echo "$a<br>";
+    for ($a = 2; $a <= $x; $a++) {
+    $c=0;
+            for ($b = 2; $b <= $a; $b++) {
+            if (($a % $b) == 0 && $a != $b) {
+            $c=1;}
+        }
+        if($c== 0){
+            echo "$a<br>";
         }
     }
     ?>
